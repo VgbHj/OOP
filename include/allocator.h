e2f6301b7aa081332819b3dff7a05cb98b57f123
Parfenov Mikhail <mishaslsk@gmail.com>
2023-12-21 18:12:23 +0300

    laba5 steal

--- include/allocator.h
+++ include/allocator.h
@@ -1,85 +1,102 @@
-#pragma once
+#ifndef MY_ALLOCATOR_H_
+#define MY_ALLOCATOR_H_
 
 #include <vector>
+#include <cstdlib>
+#include <stdexcept>
 #include <type_traits>
 
-template<class T, int BLOCKS_COUNT = 100>
-class Allocator
-{
-private:
-    void *_allocatorMemory;
-    std::vector<T *> _freeBlocks;
-    size_t _freeCount;
-public:
+namespace labWork {
+	template <typename T, int BLOCKS_COUNT = 100>
+	class Allocator {
+	private:
+		void * allocatorMemory;
+		std::vector <T *> freeBlocks;
+		std::size_t freeCount;
+	public:
 		using pointer = T *;
-        using const_pointer = const T *;
-        using void_pointer = void *;
-        using const_void_pointer = const void *;
+		using const_pointer = const T *;
+		using void_pointer = void *;
+		using const_void_pointer = const void *;
 
-        using value_type = T;
-        using reference_type = T &;
-        using const_reference_type = const T &;
+		using value_type = T;
+		using reference_type = T &;
+		using const_reference_type = const T &;
 
-        using size_type = std::size_t;
-        using difference_type = std::ptrdiff_t;
+		using size_type = std::size_t;
+		using difference_type = std::ptrdiff_t;
 
-        template <typename U>
-        struct rebind {
-            using other = Allocator<U>;
-        };
+		template <typename U>
+		struct rebind {
+			using other = Allocator<U>;
+		};
 
-        Allocator() {
-            _allocatorMemory = malloc(sizeof(T) * BLOCKS_COUNT);
+		Allocator() {
+			allocatorMemory = malloc(sizeof(T) * BLOCKS_COUNT);
 
-            for (int i = 0; i < BLOCKS_COUNT; ++i) {
-                _freeBlocks.push_back(
-                    static_cast<T *>(_allocatorMemory + i * sizeof(T)));
-            }
+			for (int i = 0; i < BLOCKS_COUNT; ++i) {
+				freeBlocks.push_back(static_cast<T *>(allocatorMemory + i * sizeof(T)));
+			}
 
-            _freeCount = BLOCKS_COUNT;
-        }
-        ~Allocator(){
-            if (_freeCount < BLOCKS_COUNT) {
-                std::cerr << "allocator: Memory leak"
-                        "allocator's dectructor was called"
-                        << std::endl;
-            }
+			freeCount = BLOCKS_COUNT;
+		}
 
-            free(_allocatorMemory);
-        }
+		~Allocator() {
+			if (freeCount < BLOCKS_COUNT) {
+				std::cerr << "WARNING: not all objects was deallocated when allocator's dectructor was called" << std::endl;
+			}
 
-        T* allocate(size_t n) {
-            T* result = nullptr;
-            if (_freeCount > 0) {
-                auto lastFree = --std::end(_freeBlocks);
+			free(allocatorMemory);
+		}
 
-                result = *lastFree;
+		pointer allocate(size_type n = 1) {
+			pointer result = nullptr;
+			if (freeCount >= n) {
+				auto lastFree = --std::end(freeBlocks);
 
-                _freeBlocks.erase(lastFree);
-                --freeCount;
-            } else {
-                throw std::bad_alloc();
-            }
+				result = *lastFree;
 
-            return result;
-        }
+				freeBlocks.erase(lastFree);
+				--freeCount;
+			}
+			else {
+				throw std::bad_alloc();
+			}
 
-        void deallocate(T* p, size_T) noexcept {
-            freeBlocks.push_back(p);
-            ++freeCount;
-        }
+			return result;
+		}
 
-        template <typename U, typename... Args>
-        void construct(U *p, Args &&...args) {
-            if (not(p >= allocatorMemory and
-                    p < allocatorMemory + BLOCKS_COUNT * sizeof(T))) {
-                throw std::invalid_argument(
-                    "Allocator::construct error: given pointer must point on "
-                    "allocator memory");
-            }
+		void deallocate(pointer p, size_type n = 1) noexcept {
+			freeBlocks.push_back(p);
+			++freeCount;
+		}
 
-            new (p) U(std::forward<Args>(args)...);
-        }
+		template<typename U, typename... Args>
+		void construct(U * p, Args && ...args) {
+			if (not(p >= allocatorMemory and p < allocatorMemory + BLOCKS_COUNT * sizeof(T))) {
+				throw std::invalid_argument("Allocator::construct error: given pointer must point on allocator memory");
+			}
 
-        void destroy(pointer p) { p->~T(); }
-};
+			new (p) U(std::forward<Args>(args)...);
+		}
+
+		void destroy(pointer p) {
+			p->~T();
+		}
+	};
+
+	template <typename T, typename U>
+	constexpr bool operator==(Allocator<T> & lhs, Allocator<U> & rhs) {
+		return &lhs == &rhs;
+	}
+
+	template <typename T, typename U>
+	constexpr bool operator!=(Allocator<T> & lhs, Allocator<U> & rhs) {
+		return not(lhs == rhs);
+	}
+
+
+} // namespace myAlloc
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+#include <map>
 
-using namespace std;
+#include "stack.h"
+#include "allocator.h"
+
+using myMap = std::map<int, int, std::less<int>, labWork::Allocator<std::pair<const int, int>>>;
+using myStack = labWork::stack<std::pair<int, int>, labWork::Allocator<std::pair<int, int>>>;
+
+
+unsigned long long int factorial(int n) {
+	if (n > 0) {
+		return n * factorial(n - 1);
+	}
+	return 1;
+}
 
 int main() {
-    
+	std::cout << "Map with custom allocator" << std::endl;
+
+	myMap map;
+	for (int i = 0; i < 10; ++i) {
+		map[i] = factorial(i);
+	}
+
+	for (auto & element : map) {
+		std::cout << element.first << ' ' << element.second << std::endl;
+	}
+
+	std::cout << "Custom stack with custom allocator" << std::endl;
+
+	myStack stack;
+	for (int i = 0; i < 10; ++i) {
+		stack.push(std::make_pair(i, factorial(i)));
+	}
+
+	for (auto & element : stack) {
+		std::cout << element.first << ' ' << element.second << std::endl;
+	}
+
+	return 0;
 }
